3543c91819ecab5905a318d4f777aa414967f696
you@example.com <you@example.com>
2025-11-06 10:32:11 +0200

    2.uzd

---
+++ ticket-price.js
@@ -0,0 +1,12 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if (visitorAge <= 6) {
+  console.log("Ieeja ir bezmaksas");
+}
+if ((isStudent = true)) {
+  console.log("biļetes cena ir 5 EUR");
+} 
+else {
+    console.log("biļetes cena ir 10 EUR.")
+}
